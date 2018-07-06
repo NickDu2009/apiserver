ed0a62ca5edd357c2a8d64a945023601e55bbb3d
PureDu <dxbinlive@gmail.com>
2018-07-06 22:46:25 +0800

    demo01

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/NickDu2009/apiserver/router"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+	"errors"
+)
+
+func main() {
+	g := gin.New()
+
+	middlewares := []gin.HandlerFunc{}
+
+	router.Load(
+		g,
+		middlewares...,
+	)
+
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.", err)
+		}
+
+		log.Print("The router has been deployed successfully")
+	}()
+
+	log.Printf("Start to listening the incoming request on http address: %s", ":8080")
+	log.Printf(http.ListenAndServe(":8080", g).Error())
+}
+
+func pingServer() error {
+	for i := 0; i < 2; i++ {
+		resp, err := http.Get("http://127.0.0.1:8080" + "sd/health")
+		if err != nil && resp.StatusCode == 200 {
+			return nil
+		}
+
+		log.Print("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router")
+}
